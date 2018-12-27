d099a920db966acf6c21141bff9b719ae3525aee
Adib Al-Amir <adibalamir@hotmail.com>
2018-12-27 13:33:36 -0500

    Created project, added a heading and  set up a countdown timer.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// class TimerInput extends Component {
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// }
+
+class Timer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.minutes}:{this.props.seconds}</h1>
+      </div>
+    )
+  }
+}
+
+class TimerControls extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.start} >Start</button>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: 125
+    }
+  }
+
+  updateTime = () => {
+    return this.setState({
+      time: this.state.time - 1
+    })
+  }
+
+  start = () => {
+    setInterval(
+      this.updateTime,
+      1000
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>Pomodoro</header>
+        <Timer minutes={Math.floor(this.state.time / 60)} seconds={this.state.time % 60} />
+        <TimerControls start={this.start} />
       </div>
     );
   }
