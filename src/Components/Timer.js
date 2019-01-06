82441f340b7dd84c91a727446fc77eb6d21d74b2
Adib Al-Amir <adibalamir@hotmail.com>
2019-01-05 19:56:33 -0500

    Changed font to Quicksand, shortened width of timer.

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -5,21 +5,15 @@ class Timer extends Component {
   render() {
     if (this.props.seconds < 10 && this.props.minutes < 10) {
       return (
-        <div>
-          <h1 id="timer">0{this.props.minutes}:0{this.props.seconds}</h1>
-        </div>
+        <h1 id="timer">0{this.props.minutes}:0{this.props.seconds}</h1>
       )
     } else if (this.props.seconds < 10) {
       return (
-        <div>
-          <h1 id="timer">{this.props.minutes}:0{this.props.seconds}</h1>
-        </div>
+        <h1 id="timer">{this.props.minutes}:0{this.props.seconds}</h1>
       )
     } else {
       return (
-        <div>
-          <h1 id="timer">{this.props.minutes}:{this.props.seconds}</h1>
-        </div>
+        <h1 id="timer">{this.props.minutes}:{this.props.seconds}</h1>
       )
     }
   }
